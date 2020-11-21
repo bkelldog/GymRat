91fecfba2b13828a17b887ac2b22f480dc565870
Bill Kelly <55664227+bkelldog@users.noreply.github.com>
2020-11-21 14:33:02 -0500

    Update User.java

--- Java/User.java
+++ Java/User.java
@@ -16,6 +16,8 @@ public class User {
     private ArrayList<String> gymIDs = new ArrayList<String>();
     private ArrayList<String> brotherList = new ArrayList<String>();
 
+    public User (){}
+
     public User(String firstName)
     {this.firstName = firstName;}
 
